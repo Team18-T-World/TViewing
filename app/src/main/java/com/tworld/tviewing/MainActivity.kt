d96bd363d85e491f62c078c4ce9c15b7301370d4
Seulbi-Lee-project <ifssfws@gmail.com>
2023-09-27 13:27:27 +0900

    [FEAT] : ⚙️: main tab 연결

--- app/src/main/java/com/tworld/tviewing/MainActivity.kt
+++ app/src/main/java/com/tworld/tviewing/MainActivity.kt
@@ -3,12 +3,32 @@ package com.tworld.tviewing
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tviewing.R
+import com.example.tviewing.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val tabLayout = binding.mainTabLayout
+        val viewPager = binding.frameLayout
+
+        // adapter 준비 및 연결
+        val adapter = TabPagerAdapter(this)
+        viewPager.adapter = adapter
+
+        // TabLayout, ViewPager 연결
+        TabLayoutMediator(
+            tabLayout, viewPager
+        ) { tab, position ->
+            when(position){
+                0 -> tab.setIcon(R.drawable.ic_home)
+                1 -> tab.setIcon(R.drawable.ic_search)
+                2 -> tab.setIcon(R.drawable.ic_user)
+            }
+        }.attach()
     }
-
-
 }
