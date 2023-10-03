d9064e8fe1bf39e86bba8cf4ba1ee402f2fdde2c
윤승재 <sjyun011278@gmail.com>
2023-10-03 22:29:45 +0900

    [RENAME] : ✏️: MainSubjectItemBinding 파일명 변경으로 인한 코드 변경

--- app/src/main/java/com/tworld/tviewing/home/HomeAdapter.kt
+++ app/src/main/java/com/tworld/tviewing/home/HomeAdapter.kt
@@ -3,11 +3,11 @@ package com.tworld.tviewing.home
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.tviewing.databinding.MainSubjectItemBinding
+import com.example.tviewing.databinding.MainMenuItemBinding
 
 class HomeAdapter : RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
 
-    class HomeViewHolder(private val binding: MainSubjectItemBinding) :
+    class HomeViewHolder(val binding: MainMenuItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         init {
 
@@ -15,10 +15,11 @@ class HomeAdapter : RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
-        return HomeViewHolder(MainSubjectItemBinding.inflate(LayoutInflater.from(parent.context)))
+        return HomeViewHolder(MainMenuItemBinding.inflate(LayoutInflater.from(parent.context)))
     }
 
     override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        //mainsubject에 있는 어댑터를 가져와서 연결
 
     }
 
