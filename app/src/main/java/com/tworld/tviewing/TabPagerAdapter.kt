97c9b9501ad1294734a6d9552055cc5d44ed732b
Seulbi-Lee-project <ifssfws@gmail.com>
2023-10-05 13:37:00 +0900

    [FEAT] : ⚙️: home 기능 추가
    news, game, entertainment 동영상 가져오기

--- app/src/main/java/com/tworld/tviewing/TabPagerAdapter.kt
+++ app/src/main/java/com/tworld/tviewing/TabPagerAdapter.kt
@@ -3,8 +3,8 @@ package com.tworld.tviewing
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.yourmediaapp_18.myVideo.MyVideoFragment
 import com.tworld.tviewing.home.HomeFragment
+import com.tworld.tviewing.myVideo.MyVideoFragment
 import com.tworld.tviewing.search.SearchFragment
 
 class TabPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
